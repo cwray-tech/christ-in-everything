66adf6717b3fa6f77678de64d0d10ecd0834b7e3
Christopher Wray <christophertwray@gmail.com>
2025-07-01 17:48:28 -0700

    refactor: update project name from "Payload Website Template" to "Christ in Everything" across documentation, metadata, and components

--- src/app/(frontend)/search/page.tsx
+++ src/app/(frontend)/search/page.tsx
@@ -1,12 +1,11 @@
 import type { Metadata } from 'next/types'
 
+import { CardPostData } from '@/components/Card'
 import { CollectionArchive } from '@/components/CollectionArchive'
+import { Search } from '@/search/Component'
 import configPromise from '@payload-config'
 import { getPayload } from 'payload'
-import React from 'react'
-import { Search } from '@/search/Component'
 import PageClient from './page.client'
-import { CardPostData } from '@/components/Card'
 
 type Args = {
   searchParams: Promise<{
@@ -83,6 +82,6 @@ export default async function Page({ searchParams: searchParamsPromise }: Args) 
 
 export function generateMetadata(): Metadata {
   return {
-    title: `Payload Website Template Search`,
+    title: `Christ in Everything Search`,
   }
 }
