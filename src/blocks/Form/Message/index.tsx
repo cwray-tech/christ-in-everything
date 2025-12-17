b79ae7c3e934296de0ee6c7707c49b5143a156ad
Christopher Wray <chris@sol.company>
2025-12-16 17:04:39 -0800

    refactor: update RichText component usage to ensure type safety with DefaultTypedEditorState in Form and RelatedPosts components

--- src/blocks/Form/Message/index.tsx
+++ src/blocks/Form/Message/index.tsx
@@ -1,13 +1,14 @@
 import RichText from '@/components/RichText'
 import React from 'react'
 
-import { Width } from '../Width'
+import type { DefaultTypedEditorState } from '@payloadcms/richtext-lexical'
 import { SerializedEditorState } from '@payloadcms/richtext-lexical/lexical'
+import { Width } from '../Width'
 
 export const Message: React.FC<{ message: SerializedEditorState }> = ({ message }) => {
   return (
     <Width className="my-12" width="100">
-      {message && <RichText data={message} />}
+      {message && <RichText data={message as DefaultTypedEditorState} />}
     </Width>
   )
 }
